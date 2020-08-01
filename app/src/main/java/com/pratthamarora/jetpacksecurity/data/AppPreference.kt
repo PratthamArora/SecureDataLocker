be9cc8138e5283e704a48db6e3e9d21695b10ea8
PratthamArora <arora.prattham@gmail.com>
2020-08-01 20:32:59 +0530

    setup App Preference class

--- app/src/main/java/com/pratthamarora/jetpacksecurity/data/AppPreference.kt
+++ app/src/main/java/com/pratthamarora/jetpacksecurity/data/AppPreference.kt
@@ -11,4 +11,18 @@ class AppPreference(private val prefs: SharedPreferences) {
         const val KEY_ACCESS_TOKEN = "PREF_KEY_ACCESS_TOKEN"
 
     }
+
+    fun setUserName(name: String) = prefs.edit().putString(KEY_USER_NAME, name).apply()
+
+    fun getUserName(): String? = prefs.getString(KEY_USER_NAME, "")
+
+    fun setUserEmail(Email: String) = prefs.edit().putString(KEY_USER_EMAIL, Email).apply()
+
+    fun getUserEmail(): String? = prefs.getString(KEY_USER_EMAIL, "")
+
+    fun setMasterKey(masterKey: String) =
+        prefs.edit().putString(KEY_ACCESS_TOKEN, masterKey).apply()
+
+    fun getMasterKey(): String? = prefs.getString(KEY_ACCESS_TOKEN, "")
+
 }
