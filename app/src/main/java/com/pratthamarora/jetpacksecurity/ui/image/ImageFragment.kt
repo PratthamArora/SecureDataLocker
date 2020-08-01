6dddc50946db8f8683321d4b34faf2a9775514e6
PratthamArora <arora.prattham@gmail.com>
2020-08-01 23:52:56 +0530

    save encrypted image

--- app/src/main/java/com/pratthamarora/jetpacksecurity/ui/image/ImageFragment.kt
+++ app/src/main/java/com/pratthamarora/jetpacksecurity/ui/image/ImageFragment.kt
@@ -1,25 +1,101 @@
 package com.pratthamarora.jetpacksecurity.ui.image
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.pratthamarora.jetpacksecurity.R
+import com.pratthamarora.jetpacksecurity.util.Utility
+import kotlinx.android.synthetic.main.fragment_image.*
 
 class ImageFragment : Fragment() {
+    companion object {
+        private const val IMAGE_CODE = 1012
+        private const val REQ_CODE = 1011
+    }
 
-    private lateinit var viewModel: ImageViewModel
+    private val viewModel by viewModels<ImageViewModel>()
+    private val permission = arrayOf(Manifest.permission.CAMERA)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(ImageViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_image, container, false)
 
-        return root
+        return inflater.inflate(R.layout.fragment_image, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+
+        fb_capture_image.setOnClickListener {
+            if (requestPermission()) {
+                getImage()
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), permission, REQ_CODE)
+            }
+        }
+    }
+
+    private fun getImage() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(requireContext().packageManager)?.also {
+                startActivityForResult(intent, IMAGE_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val bmp = data?.extras?.get("data") as Bitmap
+                viewModel.bitmap.value = bmp
+                viewModel.saveEncryptedBitmap()
+            }
+        }
+    }
+
+    private fun requestPermission() = permission.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQ_CODE) {
+            if (requestPermission()) {
+                getImage()
+            } else {
+                viewModel.snackBarMsg.value = "Permission not granted!"
+            }
+        }
+    }
+
+
+    private fun observeViewModel() {
+        viewModel.snackBarMsg.observe(viewLifecycleOwner, Observer {
+            Utility.displaySnackBar(it, requireView())
+        })
+
+        viewModel.bitmap.observe(viewLifecycleOwner, Observer {
+            imageView.setImageBitmap(it)
+        })
     }
 }
--- app/src/main/java/com/pratthamarora/jetpacksecurity/ui/image/ImageViewModel.kt
+++ app/src/main/java/com/pratthamarora/jetpacksecurity/ui/image/ImageViewModel.kt
@@ -1,7 +1,53 @@
 package com.pratthamarora.jetpacksecurity.ui.image
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.graphics.Bitmap
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.pratthamarora.jetpacksecurity.util.EncryptionHelper
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ImageViewModel : ViewModel() {
+class ImageViewModel(application: Application) : AndroidViewModel(application) {
+    val snackBarMsg: MutableLiveData<String> = MutableLiveData()
+    private val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    private val context = application
+    private val dir = File(context.filesDir, "images")
+    val bitmap: MutableLiveData<Bitmap> = MutableLiveData()
+
+    fun saveEncryptedBitmap() {
+        viewModelScope.launch {
+            val scaledBitmap = Bitmap.createScaledBitmap(bitmap.value!!, 1080, 780, true)
+            val date = Date()
+            val fileName = "EncryptedImage ${dateFormat.format(date)}.jpg"
+            if (!dir.exists()) {
+                dir.mkdir()
+            }
+            val file = File(dir, fileName)
+
+            launch(IO) {
+                try {
+                    val encryptedFile = EncryptionHelper.getEncryptedFile(file, context)
+                    val byteArray = ByteArrayOutputStream()
+                    scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArray)
+                    encryptedFile.openFileOutput().also {
+                        it.write(byteArray.toByteArray())
+                        it.flush()
+                        it.close()
+                    }
+
+                    snackBarMsg.postValue("File saved successfully!")
+                } catch (e: Exception) {
+                    snackBarMsg.postValue(e.message)
+                }
+            }
+        }
+    }
+
 
 }
