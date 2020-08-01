fafd572e144962a7b3d976b9168252455135f40c
PratthamArora <arora.prattham@gmail.com>
2020-08-01 20:37:32 +0530

    setup user repository

--- app/src/main/java/com/pratthamarora/jetpacksecurity/data/UserRepository.kt
+++ app/src/main/java/com/pratthamarora/jetpacksecurity/data/UserRepository.kt
@@ -2,4 +2,17 @@ package com.pratthamarora.jetpacksecurity.data
 
 class UserRepository(private val appPreference: AppPreference) {
 
+    fun saveUserData(name: String, email: String) {
+        appPreference.apply {
+            setUserName(name)
+            setUserEmail(email)
+        }
+    }
+
+    fun getUserName() = appPreference.getUserName()
+    fun getUserEmail() = appPreference.getUserEmail()
+
+    fun setMasterKey(key: String) = appPreference.setMasterKey(key)
+    fun getMasterKey() = appPreference.getMasterKey()
+
 }
